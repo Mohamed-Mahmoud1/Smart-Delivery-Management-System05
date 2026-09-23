c95ceec5cbde53c8dac0a496e6a86fccf0f3dd78
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-09-23 23:32:31 +0300

    Add project files.

---
+++ Smart Delivery Management System05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Smart_Delivery_Management_System05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
