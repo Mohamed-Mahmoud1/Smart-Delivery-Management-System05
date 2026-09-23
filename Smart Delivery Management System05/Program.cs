c4e7940775c804720c20c317839797f1db918461
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-09-23 23:53:44 +0300

    Part 01 — Theoretical Questions-Question01

--- Smart Delivery Management System05/Program.cs
+++ Smart Delivery Management System05/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part 01 — Theoretical Questions
+
+
+            #region Question01
+
+            //a) What happens when you assign one object variable to another object variable?
+            //Answer:The object variable copies the reference of the other object variable, and both variables point to the same object in the heap.
+
+            //b) Does assigning one object to another create a new object? Explain.
+            // Answer: No. Assigning one object variable to another just copies the reference, meaning both variables will point to the same existing object in the heap rather than creating a new one.
+
+            //c) What is the difference between copying an object and copying its reference?
+            //Answer:Copying an object creates a new one in the heap with the same values, while copying a reference means just pointing to the address of the object in the heap.
+
+            #endregion
+
+
+
+
+
+            #endregion
+
+
         }
     }
 }
